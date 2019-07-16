20d2b3d0767b81dc754363b397cf4fd67c90ffb7
Alex Junior <alexjvm90@gmail.com>
2019-07-15 23:43:47 -0300

    criar relógio

---
+++ script.js
@@ -0,0 +1,22 @@
+function relogio(){
+    let data = new Date();
+    let horas = data.getHours();
+    let minutos = data.getMinutes();
+    let segundos = data.getSeconds();
+
+    if(horas < 10){
+        horas = "0" + horas;
+    }
+
+    if(minutos < 10){
+        minutos = "0" + minutos;
+    }
+
+    if(segundos < 10){
+        segundos = "0" + segundos;
+    }
+
+    document.getElementById("relogio").innerHTML = horas + ":" + minutos + ":" + segundos;
+}
+
+window.setInterval("relogio()", 1000)
